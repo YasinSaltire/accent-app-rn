1f7aa5c7b910b87578dcbdb4c885495150c65427
danielsaltire <63422466+gueyman@users.noreply.github.com>
2023-06-05 12:15:53 -0700

    restore lost edits to incorrect choice generation. Should no longer have repeats

--- src/util/generateNIncorrectChoicesGivenOneCorrect.ts
+++ src/util/generateNIncorrectChoicesGivenOneCorrect.ts
@@ -14,20 +14,18 @@ const generateNIncorrectChoicesGivenOneCorrect = <T>(
     let idx: number = pickRandomIndexFromArray(data);
     let currentPick = data[idx];
     while (
-      currentPick.latitude == correctLat &&
-      currentPick.longitude == correctLong
+      (currentPick.latitude == correctLat &&
+      currentPick.longitude == correctLong) ||
+      ((arrayOfNIncorrect.filter(
+        (accent: any) =>
+          accent.latitude == currentPick.latitude &&
+          accent.longitude == currentPick.longitude
+      )).length > 0)
     ) {
-      if (
-        arrayOfNIncorrect.filter(
-          (accent: any) =>
-            accent.latitude != currentPick.latitude ||
-            accent.longitude != currentPick.longitude
-        )
-      ) {
         console.log(idx);
         idx = pickRandomIndexFromArray(data);
         currentPick = data[idx];
-      }
+    
     }
     //generate random index of data does not conflict with correctLat and correctLong
     arrayOfNIncorrect.push(currentPick);
