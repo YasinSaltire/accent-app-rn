c282339435ee570dfcb0a25b9b14a6c0c86226ea
YasinK <yasink@saltire.com>
2023-07-06 12:27:25 -0700

    Refactor string concat to string literal

--- src/util/generateAudioLink.ts
+++ src/util/generateAudioLink.ts
@@ -1,7 +1,7 @@
 const generateAudioLink = (
   filename: string,
 ) => {
-    return "https://saltire.com/speech/server/upload/" + filename + ".mp3"
+    return `https://saltire.com/speech/server/upload/${filename}.mp3`
 };
 
 export default generateAudioLink;
