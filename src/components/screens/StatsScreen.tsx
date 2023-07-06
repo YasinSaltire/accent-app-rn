d3310cf5c53e6cc5befeb10e2c6d9d8b597a4313
YasinK <yasink@saltire.com>
2023-07-06 11:40:17 -0700

    Remove comments, unused imports, and format file

--- src/components/screens/StatsScreen.tsx
+++ src/components/screens/StatsScreen.tsx
@@ -1,63 +1,60 @@
 import { Pressable, View, Text, StyleSheet } from "react-native";
-import generateAccentString from "../../util/generateAccentString";
 import { useEffect, useState } from "react";
 import { readData } from "../../util/AsyncStorage/storeChoice";
 import { storageKeyStrings } from "../../constants/constants";
 
 type StatScreenProps = {
-  handleGoToHome: any
-}
+  handleGoToHome: any;
+};
 
 const StatsScreen = (props: StatScreenProps) => {
-  const {handleGoToHome} = props;
-  //addDataToCurrentValue(storageKeyStrings.firstChoiceCorrectScoreKey, score)
-  //console.log(readData(storageKeyStrings.firstChoiceCorrectScoreKey))
-  //deleteData("First")
-  let [numberOfQuestionsPlayed, setNumberOfQuestionsPlayed] = useState<string>("");
-  let [numberCorrectFirstChoice, setNumberCorrectFirstChoice] = useState<string>("");
+  const { handleGoToHome } = props;
+  let [numberOfQuestionsPlayed, setNumberOfQuestionsPlayed] =
+    useState<string>("");
+  let [numberCorrectFirstChoice, setNumberCorrectFirstChoice] =
+    useState<string>("");
   let [correctPercentage, setCorrectPercentage] = useState<number>(0);
-  
-    useEffect(() =>{
-      const getTotalQuestions = async (key: string) => {
-        const data = await readData(key);
-        setNumberOfQuestionsPlayed(data);
-        return await readData(key);
-      };
-      const getTotalCorrectFirstAttempt = async (key: string) => {
-        const data = await readData(key);
-        setNumberCorrectFirstChoice(data);
-        return await readData(key);
-      };
-      const calculateCorrectPercentage = async () => {
-        const total = await getTotalQuestions(
-          storageKeyStrings.questionsPlayedKey
-        );
-        const correct = await getTotalCorrectFirstAttempt(
-          storageKeyStrings.firstChoiceCorrectScoreKey
-        );
-        const correctRate = Math.floor(
-          (parseFloat(correct) / parseFloat(total)) * 100
-        );
-        setCorrectPercentage(correctRate);
-      };
 
-      calculateCorrectPercentage();
-    }, [])
+  useEffect(() => {
+    const getTotalQuestions = async (key: string) => {
+      const data = await readData(key);
+      setNumberOfQuestionsPlayed(data);
+      return await readData(key);
+    };
+    const getTotalCorrectFirstAttempt = async (key: string) => {
+      const data = await readData(key);
+      setNumberCorrectFirstChoice(data);
+      return await readData(key);
+    };
+    const calculateCorrectPercentage = async () => {
+      const total = await getTotalQuestions(
+        storageKeyStrings.questionsPlayedKey
+      );
+      const correct = await getTotalCorrectFirstAttempt(
+        storageKeyStrings.firstChoiceCorrectScoreKey
+      );
+      const correctRate = Math.floor(
+        (parseFloat(correct) / parseFloat(total)) * 100
+      );
+      setCorrectPercentage(correctRate);
+    };
 
+    calculateCorrectPercentage();
+  }, []);
 
   return (
     <View style={playScreenStyles("black")}>
-      
-        <Text style={{marginTop: '5%', marginBottom: '5%', color: "white" }}>
-          Lifetime questions answered: {numberOfQuestionsPlayed}{" "}
-        </Text>
-        <Text style={{ marginBottom: '5%',color: "white" }}>
-          Total correct first attempts: {numberCorrectFirstChoice}{" "}
-        </Text>
+      <Text style={{ marginTop: "5%", marginBottom: "5%", color: "white" }}>
+        Lifetime questions answered: {numberOfQuestionsPlayed}{" "}
+      </Text>
+      <Text style={{ marginBottom: "5%", color: "white" }}>
+        Total correct first attempts: {numberCorrectFirstChoice}{" "}
+      </Text>
 
-        <Text style={{ marginBottom: '10%',color: "white" }}>
-          First attempt correct rate: {correctPercentage ? correctPercentage : "-"}%{" "}
-        </Text>
+      <Text style={{ marginBottom: "10%", color: "white" }}>
+        First attempt correct rate:{" "}
+        {correctPercentage ? correctPercentage : "-"}%{" "}
+      </Text>
       <Pressable
         style={{
           marginTop: "10%",
@@ -90,7 +87,7 @@ const playScreenStyles = (color: string) => {
       alignItems: "center",
       flexDirection: "column",
       flex: 1,
-      justifyContent: 'center'
+      justifyContent: "center",
     },
   });
   return style.default;
