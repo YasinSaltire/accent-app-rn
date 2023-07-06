dc7f9b6331d202d817a4bb26929968b0b011f7c2
YasinK <yasink@saltire.com>
2023-07-06 11:42:42 -0700

    Remove gramround class (unused)

--- src/model/GameRound.ts
+++ src/model/GameRound.ts
@@ -7,28 +7,4 @@ interface Round {
   setQnACollection(qac: QnACollection): void;
 }
 
-class GameRound implements Round {
-  private id: number;
-  private qnaCollection: QnACollection;
-  constructor(id: number, qac: QnACollection) {
-    this.id = id;
-    this.qnaCollection = qac;
-  }
-  getId(): number {
-    return this.id;
-  }
-
-  setId(id: number): void {
-    this.id = id;
-  }
-
-  getQnACollection(): QnACollection {
-    return this.qnaCollection;
-  }
-
-  setQnACollection(qac: QnACollection): void {
-    this.qnaCollection = qac;
-  }
-}
-
 export default Round;
